bfcd07c33e30c4b24713ff2451b1219f1354a9e8
Brooklyn Veasey <10966687@my.uvu.edu>
2023-03-18 21:39:17 -0600

    Added floor to 2D platformer
    lol

--- 2D Platformer Game/Assets/Scripts/GameManager.cs
+++ 2D Platformer Game/Assets/Scripts/GameManager.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public int collectible;
-    public TextMeshProUGUI scoreText;
+    //public TextMeshProUGUI scoreText;
  
     public void IncreaseScore(int amount)
     {
-        collectible += amount
+      //  collectible += amount
     }
 
 }
